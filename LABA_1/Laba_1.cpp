151fe1fea2aed418520bda099119e4a5de29dd43
SondeyS <sanya4a1j@gmail.com>
2018-12-10 15:07:06 +0200

    Create Laba_1.cpp

---
+++ LABA_1/Laba_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+main()
+{
+	int num;
+
+	cout << "Table multiplication by ";
+	cin >> num;
+	cout << endl;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		if (i<10) {
+			cout << i << " * " << num << " = " << i * num << endl;
+		}
+		else
+			cout << i << "* " << num << " = " << i * num << endl;
+	}
+
+	return 0;
+}
